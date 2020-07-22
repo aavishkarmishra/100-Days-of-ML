4da7293adc0c6f940fe0f2fb0749b500277daec8
Aavishkar Mishra <aavishkarmishra@gmail.com>
2020-07-22 23:49:44 +0530

    cloned starter code

--- tools/startup.py
+++ tools/startup.py
@@ -37,11 +37,11 @@ print ("download complete!")
 
 
 print
-print "unzipping Enron dataset (this may take a while)"
+print ("unzipping Enron dataset (this may take a while)")
 import tarfile
 import os
 os.chdir("..")
 tfile = tarfile.open("enron_mail_20150507.tar.gz", "r:gz")
 tfile.extractall(".")
 
-print "you're ready to go!"
+print ("you're ready to go!")
