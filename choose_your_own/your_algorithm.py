a0ca32fb00a3b3406221120884955079f29fea5a
Aavishkar Mishra <aavishkarmishra@gmail.com>
2020-07-30 00:14:36 +0530

    Finished lecture.5 (choose your algorithm)

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-
+import sys
+from time import time
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
@@ -30,13 +31,49 @@ plt.show()
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 
+print("Starting K-nearest neighbors.........")
+clf1 = KNeighborsClassifier(n_neighbors= 1)
 
+t1_k = time()
+clf1.fit(features_train,labels_train)
+print("Time to train : ", round(time()-t1_k,3))
 
+t2_k = time()
+pred1 = clf1.predict(features_test)
+print("Time to predict : ", round(time()-t2_k,3))
 
+print("Accuracy of K-nearest neighbors : ",accuracy_score(labels_test,pred1)*100, "%" )
 
+print("Starting AdaBoost.........")
+clf2 = AdaBoostClassifier(n_estimators=11)
 
+t1_Ada = time()
+clf2.fit(features_train,labels_train)
+print("Time to train : ", round(time()-t1_Ada,3))
 
+t2_Ada = time()
+pred2 = clf2.predict(features_test)
+print("Time to predict : ", round(time()-t2_Ada,3))
+
+print("Accuracy of AdaBoost : ",round(accuracy_score(labels_test,pred2)*100,3), "%" )
+
+print("Starting Random Forest.........")
+clf3 = RandomForestClassifier(max_depth=3,n_estimators=100)
+
+t1_rf = time()
+clf3.fit(features_train,labels_train)
+print("Time to train : ", round(time()-t1_rf,3))
+
+t2_rf = time()
+pred3 = clf3.predict(features_test)
+print("Time to predict : ", round(time()-t2_rf,3))
+
+print("Accuracy of Random Forest : ",round(accuracy_score(labels_test,pred3)*100,3), "%" )
 
 try:
     prettyPicture(clf, features_test, labels_test)
